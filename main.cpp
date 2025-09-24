e123401bad25a45bb6b21b7eeae32167f7bcf79d
Vincent-Khor <khor25204weikang@gmail.com>
2025-09-24 19:27:47 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 int main() {
-    cout << "Grab Fare Estimator Program" << endl;
+    int service;
+    cout << "Choose your service:\n";
+    cout << "1. GrabCar\n";
+    cout << "2. GrabBike\n";
+    cout << "3. GrabFood\n";
+    cout << "Enter choice: ";
+    cin >> service;
+
+    cout << "You chose service: " << service << endl;
     return 0;
 }
-
